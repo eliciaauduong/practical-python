0523d2c6b22ba0eb5d11bd3fcaf0b06dc8d9b7ad
Elicia Au Duong <eliciaauduong@outlook.com>
2020-08-08 16:34:55 +1000

    completed exercise 1.5

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,9 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+bounce_count = 0
+while bounce_count < 10:
+    height *= 0.6
+    print(bounce_count + 1, round(height, ndigits = 4))
+    bounce_count += 1
