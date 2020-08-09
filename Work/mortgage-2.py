f7a88110623d84e6095160757cbaf9b76234fa0a
Elicia Au Duong <eliciaauduong@outlook.com>
2020-08-09 22:24:10 +1000

    completed exercise 1.8

---
+++ Work/mortgage-2.py
@@ -0,0 +1,20 @@
+# exercise 1.9
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+extra_payment = 1000
+month = 0
+
+while principal > 0:
+    if month < 12:
+        principal = principal * (1 + rate/12) - (payment + extra_payment)
+        total_paid = total_paid + payment + extra_payment
+    else:
+        principal = principal * (1 + rate/12) - payment
+        total_paid = total_paid + payment
+    month += 1
+
+print('Total paid', total_paid)
+print('Number of months required', month)
