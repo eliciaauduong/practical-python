5f48f17ae6c97415766fa326385453b6e6d6fff5
Elicia Au Duong <eliciaauduong@outlook.com>
2020-08-10 11:18:18 +1000

    completed exercise 1.15

---
+++ Work/membership.py
@@ -0,0 +1,11 @@
+# exercise 1.15
+
+symbols = 'HPQ,AAPL,IBM,MSFT,YHOO,SCO,GOOG'
+
+'IBM' in symbols  # True
+
+'AA' in symbols  # True
+# 'AA' returns true because it is a subtring of symbol
+# it doesn't have to be a complete word to return true
+
+'CAT' in symbols  # False
