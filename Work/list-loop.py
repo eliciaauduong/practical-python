72d3058ac21ee91c71fadf94277346505bdc2c6d
Elicia Au Duong <eliciaauduong@outlook.com>
2020-08-11 21:09:11 +1000

    completed exercise 1.20

---
+++ Work/list-loop.py
@@ -0,0 +1,16 @@
+# exercise 1.20
+
+symbols = 'HPQ,AAPL,IBM,MSFT,YHOO,DOA,GOOG'
+
+symlist = symbols.split(',')
+
+for s in symlist:
+    print('s =', s)
+
+# s = HPQ
+# s = AAPL
+# s = IBM
+# s = MSFT
+# s = YHOO
+# s = DOA
+# s = GOOG
