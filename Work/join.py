4919b72d0c385f6aa9272bdfa6aba0e1b80d60e8
Elicia Au Duong <eliciaauduong@outlook.com>
2020-08-11 21:11:18 +1000

    completed exercise 1.24

---
+++ Work/join.py
@@ -0,0 +1,15 @@
+# exercise 1.24
+
+symlist = ['YHOO', 'RHT', 'HPQ', 'GOOG', 'AIG', 'AAPL', 'AA']
+
+a = ','.join(symlist)
+a
+# 'YHOO,RHT,HPQ,GOOG,AIG,AAPL,AA'
+
+b = ':'.join(symlist)
+b
+# 'YHOO:RHT:HPQ:GOOG:AIG:AAPL:AA'
+
+c = ''.join(symlist)
+c
+# 'YHOORHTHPQGOOGAIGAAPLAA'
