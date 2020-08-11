839b1726fd5f344deda33b7e37ded44fb3b4dce2
Elicia Au Duong <eliciaauduong@outlook.com>
2020-08-11 21:10:55 +1000

    completed exercise 1.23

---
+++ Work/sorting.py
@@ -0,0 +1,11 @@
+# exercise 1.23
+
+symlist = ['HPQ', 'AA', 'AAPL', 'AIG', 'GOOG', 'RHT', 'YHOO']
+
+symlist.sort()
+symlist
+# ['AA', 'AAPL', 'AIG', 'GOOG', 'HPQ', 'RHT', 'YHOO']
+
+symlist.sort(reverse=True)
+symlist
+# ['YHOO', 'RHT', 'HPQ', 'GOOG', 'AIG', 'AAPL', 'AA']
