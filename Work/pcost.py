3748c33c283b5091894d3c3502e27737240a6faa
Elicia Au Duong <eliciaauduong@outlook.com>
2020-08-12 20:37:12 +1000

    completed exercise 1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,16 @@
 # pcost.py
 #
 # Exercise 1.27
+
+# redirect python path to current directory
+import os
+os.chdir('C:\\Users\\Elicia Au Duong\\Documents\\GitHub\\practical-python\\Work')
+
+file = open('Data/portfolio.csv', 'rt')
+headers = next(file).split(',')
+cost = 0
+for line in file:
+    row = line.split(',')
+    cost = cost + (float(row[1]) * float(row[2]))
+
+print(f'Total cost {round(cost, ndigits=2)}')
